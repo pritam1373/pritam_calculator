87855c422b35f9f3f0087da1fdc1ba443d9296a4
pritam1373 <ovnandy99@gmail.com>
2025-07-05 18:44:07 +0530

    Update scripts2.js

--- scripts2.js
+++ scripts2.js
@@ -5,8 +5,7 @@ v=document.getElementById("vag")
 g=document.getElementById("gun")
 t=document.getElementById("tdp")
 c=document.getElementById("rl")
-
-
+uni=document.getElementById("container")
 
 b1=document.getElementById("box1")
 b2=document.getElementById("box2")
@@ -37,13 +36,44 @@ function fac(p){
 e.addEventListener("click",()=>{
         // alert("pritam")
         c=b1.textContent
-        d=c.replace(/×/g,"*").replace(/÷/g,"/").replace(/\%/g,"*(1/100)").replace(/sin\(([^)]+)\)/g, (_, angle) => `Math.sin((${angle}) * Math.PI / 180)`).replace(/cos\(([^)]+)\)/g, (_, angle) => `Math.cos((${angle}) * Math.PI / 180)`).replace(/tan\(([^)]+)\)/g, (_, angle) => `Math.tan((${angle}) * Math.PI / 180)`).replace(/lg\(([^)]+)\)/g, (_, val) => `Math.log10(${val})`).replace(/\^/g,"**").replace(/√/,"Math.sqrt").replace(/(\d+)!/g, 'fac($1)')
+        d=c.replace(/×/g,"*").replace(/÷/g,"/").replace(/\%/g,"*(1/100)").replace(/sin\(([^)]+)\)/g, (_, angle) => `Math.sin((${angle}) * Math.PI / 180)`).replace(/cos\(([^)]+)\)/g, (_, angle) => `Math.cos((${angle}) * Math.PI / 180)`).replace(/tan\(([^)]+)\)/g, (_, angle) => `Math.tan((${angle}) * Math.PI / 180)`).replace(/lg\(([^)]+)\)/g, (_, val) => `Math.log10(${val})`).replace(/\^/g,"**").replace(/√/,"Math.sqrt").replace(/(\d+)!/g, 'fac($1)').replace(/π/g,"3.1415926536")
         // .replace(/\!/g,"fac")
         result=eval(d)
         b1.textContent = b1.textContent + " = " + result
         
 })
+
 c.addEventListener("click", () =>{
         location.reload()
 })
 
+bot2=document.getElementById("botam2")
+bot=document.getElementById("botam")
+
+
+c1=document.getElementById("cover1")
+c2=document.getElementById("cover2")
+c3=document.getElementById("cover3")
+    
+
+bot2.addEventListener("click", () =>{
+       c1.style.transform="translateX(-500px)"
+       c1.style.transition="5s"
+        c2.style.transform="translateX(500px)"
+       c2.style.transition="5s"
+        c3.style.transform="translateY(520px)"
+       c3.style.transition="5s"
+    
+       bot.style.transform="translateY(460px)"
+       bot.style.backgroundColor="red"
+       b2.style.transition="5s"
+       bot2.innerHTML="OFF"
+       bot2.style.borderColor="red"
+       b2.style.marginTop="-40px"
+     bot2.addEventListener("click", () =>{
+        uni.style.transition="5s"
+         location.reload()
+
+     })
+
+})
